106d102a9c61cdc80e167e15671d217e02149a7d
Yejoon Lee <beeriver1@gmail.com>
2021-05-26 19:12:14 +0900

    Private LB

---
+++ private_LB/RF_submit.py
@@ -0,0 +1,52 @@
+# Random Forest
+# for private LB
+# should be run in /Solar_Predict
+
+import pickle
+import pandas as pd
+
+from privateLB import API, date_ctrl
+
+KEY = "sNfoTDclWrvFGpIEFDEXvj+EaCjLrOILF7IYehdRCcYBxnMP0zna40R1UmY6qfWBG0gJ16c3T8ManHwvhACk7w=="
+
+
+def predict(location):
+    # get data
+    data = API(BASE_DATE, BASE_TIME, location, KEY).get_data()
+
+    # load model
+    with open(f"witt_modeling/rf_models/{location}_model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    # predict
+    x = data.loc[:, X_COLS]
+    predicted = model.predict(x)
+    return predicted
+
+
+def to_submission(path, predict_date, predicted):
+    submission = pd.read_csv("original_dataset/sample_submission.csv")
+    submission.loc[submission["time"].str.contains(predict_date), "ulsan"] = predicted
+    submission.to_csv(path, index=False)
+
+
+BASE_DATE = "20210525"
+BASE_TIME = "2000"
+SHIFT = 20
+X_COLS = [
+    "Temperature",
+    "Humidity",
+    "Cloud",
+    "Day_cos",
+    "Day_sin",
+    "Year_cos",
+    "Year_sin",
+]
+PATH = f"{BASE_DATE}-{BASE_TIME}_submission"
+
+dj = predict("dangjin")
+uls = predict("ulsan")
+total = dj + uls
+
+to_submission(PATH, date_ctrl(BASE_DATE, SHIFT, "pandas"), total)
+
