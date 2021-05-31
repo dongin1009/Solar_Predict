1f980e515a489fc0d8bc27677206ab9f84521bec
Yejoon Lee <beeriver1@gmail.com>
2021-05-31 21:51:03 +0900

    Private 사전 테스트 진행

--- private_LB/RF_submit.py
+++ private_LB/RF_submit.py
@@ -10,9 +10,11 @@ from privateLB import API, date_ctrl
 KEY = "sNfoTDclWrvFGpIEFDEXvj+EaCjLrOILF7IYehdRCcYBxnMP0zna40R1UmY6qfWBG0gJ16c3T8ManHwvhACk7w=="
 
 
-def predict(location):
+def predict(location, base_date, base_time):
     # get data
-    data = API(BASE_DATE, BASE_TIME, location, KEY).get_data()
+    data = API(base_date, base_time, location, KEY).get_data(
+        preprocess=True, itp_method="quadratic"
+    )
 
     # load model
     with open(f"witt_modeling/rf_models/{location}_model.pkl", "rb") as f:
@@ -25,14 +27,41 @@ def predict(location):
 
 
 def to_submission(path, predict_date, predicted):
-    submission = pd.read_csv("original_dataset/sample_submission.csv")
+    submission = pd.read_csv("./private_LB/test_sample_submission.csv")
+    # Doesn't have to be 'ulsan'. Can be any arbitrary column.
     submission.loc[submission["time"].str.contains(predict_date), "ulsan"] = predicted
     submission.to_csv(path, index=False)
 
 
-BASE_DATE = "20210525"
-BASE_TIME = "2000"
-SHIFT = 20  # 1 in real private LB case
+def tweak_after_prediction(array, N):
+    # replace by 320 if value < 320
+    # add N if value > 320
+    for i in range(len(array)):
+        if array[i] < 320:
+            array[i] = 320
+        else:
+            array[i] += N
+    return array
+
+
+def main(base_date, base_time):
+    # path to save submission file
+    PATH = f"./witt_modeling/privateLB_submissions/rf_{base_date}-{base_time}_submission.csv"
+
+    # sum dangjin prediction and ulsan prediction
+    dj = predict("dangjin", base_date, base_time)
+    uls = predict("ulsan", base_date, base_time)
+    total = dj + uls
+
+    # add value to compensate underestimation
+    total = tweak_after_prediction(total, N)
+    print(total)
+
+    to_submission(PATH, date_ctrl(base_date, SHIFT, "pandas"), total)
+
+
+# constants
+SHIFT = 1
 X_COLS = [
     "Temperature",
     "Humidity",
@@ -42,11 +71,7 @@ X_COLS = [
     "Year_cos",
     "Year_sin",
 ]
-PATH = f"{BASE_DATE}-{BASE_TIME}_submission"
+N = 20
 
-dj = predict("dangjin")
-uls = predict("ulsan")
-total = dj + uls
-
-to_submission(PATH, date_ctrl(BASE_DATE, SHIFT, "pandas"), total)
-
+if __name__ == "__main__":
+    main("20210531", "2000")
