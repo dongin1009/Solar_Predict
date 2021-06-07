d8977fa19972aa3f12eb00572b0424e050d03764
Yejoon Lee <beeriver1@gmail.com>
2021-06-07 21:01:34 +0900

    Minor fix in Ensemble.py: only 'ulsan' columns are added from now on

--- private_LB/privateLB_Ensemble.py
+++ private_LB/privateLB_Ensemble.py
@@ -9,20 +9,23 @@ import pandas as pd
 from privateLB_models import RfPredict, LgbmPredict, XgbPredict
 
 # date of prediction
-base_date = "20210606"
-base_time = '2000'
+base_date = "20210607"
+base_time = "2000"
 
 # predictions from the models
 submission_rf = RfPredict(base_date, base_time).get_submission()
 submission_lgbm = LgbmPredict(base_date, base_time).get_submission()
-#submission_xgb = XgbPredict(base_date, base_time).get_submission()
+submission_xgb = XgbPredict(base_date, base_time).get_submission()
 
 # add each predictions
-submission_ensemble = submission_rf.add(submission_lgbm, fill_value = 0)
-#submission_ensemble = submission_ensemble.add(submission_xgb, fill_value = 0)
+# "ulsan" is an arbitrary column to store the sum of all predictions
+submission_ensemble = submission_rf
+submission_ensemble["ulsan"] = submission_ensemble["ulsan"].add(submission_lgbm["ulsan"], fill_value = 320)
+submission_ensemble["ulsan"] = submission_ensemble["ulsan"].add(submission_xgb["ulsan"], fill_value = 320)
 
-submission_ensemble["ulsan"] = submission_ensemble["ulsan"] / 3
+submission_ensemble["ulsan"] = submission_ensemble["ulsan"] / 3 
  
 # write CSV
-print(submission_ensemble)
+print(submission_ensemble.tail(48)) # show the predictions for two days(=48 rows)
 submission_ensemble.to_csv(f"./submissions/{base_date}_{base_time}.csv", index = False)
+# be careful when modifying the write_path. Unintended file in "submissions" directory might cause code to misbehave.
